33e346c57b74213b7662de0627cf0323ffdc93a5
Viv Richards <1014468+vivrichards600@users.noreply.github.com>
2021-04-19 15:20:35 +0100

    PLATUI-1088 - Updating script so it doesnt have to rely on any others as a POC.

--- run_tests.sh
+++ run_tests.sh
@@ -1,38 +1,24 @@
 #!/bin/bash -e
 
-echo "INFO: Stopping a11y container if already running"
-docker stop a11y || true
-
 echo "INFO: Building accessibility-assessment:SNAPSHOT"
-./scripts/build-local-image.sh
+PROJECT_DIR=${WORKSPACE}/accessibility-assessment
+DOCKER_FILES_DIR=${PROJECT_DIR}/docker/files
+
+# cp the latest app/ into files
+mkdir ${DOCKER_FILES_DIR}/app
+cp -r app docker/files/
+
+
+docker rm a11y
+docker build -t accessibility-assessment:SNAPSHOT ${PROJECT_DIR}/docker
 
 echo "INFO: Running accessibility-assessment:SNAPSHOT in background"
-./scripts/run-local.sh
+PROJECT_DIR=${WORKSPACE}/accessibility-assessment
 
-# Checks if accessibility-assessment is alive and can accept requests
-check_accessibility_assessment_service_running () {
-  max_iterations=5
-  iterations=0
-  while true
-  do
-    ((iterations++))
-    echo "INFO: Checking if accessibility-assessment is alive (attempt $iterations of 5)"
-    sleep 5
-
-    status=$(curl -sS  -I http://localhost:6010/api/status  2> /dev/null | head -n 1 | cut -d' ' -f2)
-
-    if [ "$status" = "200" ]; then
-      echo "INFO: Request to accessibility-assessment /api/status endpoint returned 200"
-		  break
-	  fi
-
-    if [ "$iterations" -ge "$max_iterations" ]; then
-      echo "INFO: Timeout - could not make request to accessibility-assessment service"
-      exit 1
-    fi
-  done
-}
-check_accessibility_assessment_service_running
+docker run -d \
+    --name a11y \
+    -p 6010:6010 \
+    accessibility-assessment:SNAPSHOT
 
 echo "INFO: Navigating to accessibility-assessment tests directory"
 cd tests/
