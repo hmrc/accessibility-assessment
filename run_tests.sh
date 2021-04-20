b849ed3840266fbbbe170e14b86a850a8a350d69
Viv Richards <1014468+vivrichards600@users.noreply.github.com>
2021-04-20 14:53:25 +0100

    PLATUI-1088 - Updating path to docker folder

--- run_tests.sh
+++ run_tests.sh
@@ -1,17 +1,12 @@
 #!/bin/bash -e
 
-PROJECT_DIR=${WORKSPACE}/accessibility-assessment
-DOCKER_FILES_DIR=${PROJECT_DIR}/docker/files
-
 echo "INFO: Copying the latest app/ into files"
 cp -r app docker/files/
 
 echo "INFO: Building accessibility-assessment:SNAPSHOT"
-docker build -t accessibility-assessment:SNAPSHOT ${PROJECT_DIR}/docker
+docker build -t accessibility-assessment:SNAPSHOT ${WORKSPACE}/docker
 
 echo "INFO: Running accessibility-assessment:SNAPSHOT in background"
-PROJECT_DIR=${WORKSPACE}/accessibility-assessment
-
 docker run -d \
     --name a11y \
     -p 6010:6010 \
