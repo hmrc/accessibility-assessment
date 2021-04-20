1b496c3b0387512d5b4581e0764c1f5ef8fb7275
Viv Richards <1014468+vivrichards600@users.noreply.github.com>
2021-04-20 10:44:21 +0100

    PLATUI-1088 - Removing command to remove a11y container

--- run_tests.sh
+++ run_tests.sh
@@ -5,11 +5,8 @@ PROJECT_DIR=${WORKSPACE}/accessibility-assessment
 DOCKER_FILES_DIR=${PROJECT_DIR}/docker/files
 
 # cp the latest app/ into files
-#mkdir ${DOCKER_FILES_DIR}/app
 cp -r app docker/files/
 
-
-docker rm a11y
 docker build -t accessibility-assessment:SNAPSHOT ${PROJECT_DIR}/docker
 
 echo "INFO: Running accessibility-assessment:SNAPSHOT in background"
