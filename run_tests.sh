e26b700305513669623028fbb7156a25e8bd0a8f
Viv Richards <1014468+vivrichards600@users.noreply.github.com>
2021-04-20 12:21:10 +0100

    PLATUI-1088 - Adding additional echo to figure out by path not found

--- run_tests.sh
+++ run_tests.sh
@@ -1,18 +1,12 @@
 #!/bin/bash -e
 
-echo "INFO: Building accessibility-assessment:SNAPSHOT"
 PROJECT_DIR=${WORKSPACE}/accessibility-assessment
 DOCKER_FILES_DIR=${PROJECT_DIR}/docker/files
 
-# cp the latest app/ into files
+echo "INFO: Copying the latest app/ into files"
+cp -r app docker/files/
 
-cp ${PROJECT_DIR}/app/*.js  $DOCKER_FILES_DIR/app
-cp ${PROJECT_DIR}/app/package.json  $DOCKER_FILES_DIR/app/package.json
-cp ${PROJECT_DIR}/app/package-lock.json  $DOCKER_FILES_DIR/app/package-lock.json
-cp -r ${PROJECT_DIR}/app/resources $DOCKER_FILES_DIR/app/
-cp -r ${PROJECT_DIR}/app/routes $DOCKER_FILES_DIR/app/
-cp -r ${PROJECT_DIR}/app/services $DOCKER_FILES_DIR/app/
-
+echo "INFO: Building accessibility-assessment:SNAPSHOT"
 docker build -t accessibility-assessment:SNAPSHOT ${PROJECT_DIR}/docker
 
 echo "INFO: Running accessibility-assessment:SNAPSHOT in background"
