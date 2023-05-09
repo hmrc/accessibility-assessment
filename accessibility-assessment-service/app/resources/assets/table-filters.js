f85ab2f75761302e297d4a58d97284407208db24
Viv Richards <1014468+vivrichards600@users.noreply.github.com>
2023-05-09 17:03:08 +0100

    Refactoring: - extracting markup used to create filters into function - clean up filter functions to pass event

--- accessibility-assessment-service/app/resources/assets/table-filters.js
+++ accessibility-assessment-service/app/resources/assets/table-filters.js
@@ -15,34 +15,38 @@ $(document).ready(function () {
         ],
     });
 
+    function createCheckboxFilter(elementId, label) {
+        return $(`<div class='filter'><label for='${elementId}'>${label}</label><input type='checkbox' id='${elementId}'/></div>`);
+    }
+
     const axeContainer = $("#axeTable_wrapper");
-    const axeShowOnlyErrorsFilter = $("<div class='filter'><label for='axe_show_only_errors'>Show only errors</label><input type='checkbox' id='axe_show_only_errors' data-table='axeTable'/></div>");
-    const axeHideKnownIssuesFilter = $("<div class='filter'><label for='axe_hide_known_issues'>Hide known issues</label><input type='checkbox' id='axe_hide_known_issues'/></div>");
+    const axeShowOnlyErrorsFilter = createCheckboxFilter('axe_show_only_errors', 'Show only errors');
+    const axeHideKnownIssuesFilter = createCheckboxFilter('axe_hide_known_issues', 'Hide known issues');
     axeContainer.prepend(axeShowOnlyErrorsFilter);
     axeContainer.prepend(axeHideKnownIssuesFilter);
 
-    axeShowOnlyErrorsFilter.change(function () {filterTableErrors('axe_show_only_errors', axeTable);});
-    axeHideKnownIssuesFilter.change(function () { filterTableKnownIssues('axe_hide_known_issues', axeTable);});
+    axeShowOnlyErrorsFilter.change(function (event) {filterTableErrors(event, axeTable);});
+    axeHideKnownIssuesFilter.change(function (event) { filterTableKnownIssues(event, axeTable);});
 
     const vnuContainer = $("#vnuTable_wrapper");
-    const vnuShowOnlyErrorsFilter = $("<div class='filter'><label for='vnu_show_only_errors'>Show only errors</label><input type='checkbox' id='vnu_show_only_errors'/></div>");
-    const vnuHideKnownIssuesFilter = $("<div class='filter'><label for='vnu_hide_known_issues'>Hide known issues</label><input type='checkbox' id='vnu_hide_known_issues'/></div>");
+    const vnuShowOnlyErrorsFilter = createCheckboxFilter('vnu_show_only_errors', 'Show only errors');
+    const vnuHideKnownIssuesFilter = createCheckboxFilter('vnu_hide_known_issues', 'Hide known issues');
     vnuContainer.prepend(vnuShowOnlyErrorsFilter);
     vnuContainer.prepend(vnuHideKnownIssuesFilter);
 
-    vnuShowOnlyErrorsFilter.change(function () {filterTableErrors('vnu_show_only_errors', vnuTable);});
-    vnuHideKnownIssuesFilter.change(function () {filterTableKnownIssues('vnu_hide_known_issues', vnuTable);});
+    vnuShowOnlyErrorsFilter.change(function (event) {filterTableErrors(event, vnuTable);});
+    vnuHideKnownIssuesFilter.change(function (event) {filterTableKnownIssues(event, vnuTable);});
 
-    function filterTableKnownIssues(elementId,table) {
-        const hideKnownIssuesChecked = $(`input:checkbox[id=${elementId}]`).is(':checked');
+    function filterTableKnownIssues(event, table) {
+        const hideKnownIssuesChecked = event.target.checked;
         const searchValue = hideKnownIssuesChecked ? '^$' : '';
         const regexEnabled = true;
         const caseInsensitive = false;
         table.column(6).search(searchValue, regexEnabled, caseInsensitive).draw();
     }
 
-    function filterTableErrors(elementId, table) {
-        const toggleErrorsChecked = $(`input:checkbox[id=${elementId}]`).is(':checked');
+    function filterTableErrors(event, table) {
+        const toggleErrorsChecked = event.target.checked;
         const searchValue = toggleErrorsChecked ? 'ERROR' : '';
         table.column(4).search(searchValue).draw();
     }
